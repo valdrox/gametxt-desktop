56c68491731a552af3cc5521b14fd4c044bd9c42
Patrick <13651676+valdrox@users.noreply.github.com>
2021-06-12 16:36:47 -0400

    adds sourcemaps for debugging

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,8 @@
 // vue.config.js
 module.exports = {
+  configureWebpack: {
+    devtool: "source-map",
+  },
   pluginOptions: {
     electronBuilder: {
       preload: "src/preload.js", // or 'src/preload.js'
