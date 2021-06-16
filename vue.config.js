f9a9d82a94045f3e11320e396ba2319c7178bbaf
Patrick <13651676+valdrox@users.noreply.github.com>
2021-06-16 15:45:47 -0400

    updates metadata for electron-builder options

--- vue.config.js
+++ vue.config.js
@@ -7,7 +7,19 @@ module.exports = {
     electronBuilder: {
       preload: "src/preload.js", // or 'src/preload.js'
       builderOptions: {
+        appId: "gametxt.desktop",
+        productName: "GameTxt Desktop",
         publish: ["github"],
+        mac: {
+          category: "public.app-category.education",
+        },
+        dmg: {
+          icon: false,
+        },
+        linux: {
+          target: ["AppImage"],
+          category: "Education",
+        },
       },
     },
   },
